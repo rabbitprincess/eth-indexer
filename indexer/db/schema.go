584dc185fa1b16bfb77f39509df0c826c25f38ff
rabbitprincess <sjc02183@naver.com>
2024-10-17 22:17:44 +0900

    init schema

--- indexer/db/schema.go
+++ indexer/db/schema.go
@@ -21,4 +21,87 @@ func (m *BaseEsType) SetID(id string) {
 	m.Id = id
 }
 
+type AccountBalance struct {
+	BaseEsType
+	Account        string `json:"account" db:"account"`
+	BlockNumber    uint64 `json:"block_number" db:"block_number"`
+	BlockTimestamp string `json:"block_timestamp" db:"block_timestamp"`
+	Balance        uint64 `json:"balance" db:"balance"`
+}
+
+type AccountBalanceHistory struct {
+	BaseEsType
+	Account        string `json:"account" db:"account"`
+	BlockNumber    uint64 `json:"block_number" db:"block_number"`
+	BlockTimestamp string `json:"block_timestamp" db:"block_timestamp"`
+	ChangeType     uint64 `json:"change_type" db:"change_type"`
+	ChangeBalance  uint64 `json:"change_balance" db:"change_balance"`
+	Txid           uint64 `json:"txid" db:"txid"`
+}
+
 var EsSchema map[string]string
+
+func init() {
+	EsSchema = map[string]string{}
+
+	EsSchema["account_balance"] = `{
+	"settings": {
+		"number_of_shards": 10,
+		"number_of_replicas": 1,
+		"index.max_result_window": 100000
+	},
+	"mappings": {
+		"properties": {
+			"id": {
+				"type": "keyword"
+			},
+			"account": {
+				"type": "keyword"
+			},
+			"block_number": {
+				"type": "long"
+			},
+			"block_timestamp": {
+				"type": "date"
+			},
+			"balance": {
+				"type": "long"
+			}
+		}
+	}
+}`
+
+	EsSchema["account_balance_history"] = `{
+	"settings": {
+		"number_of_shards": 10,
+		"number_of_replicas": 1,
+		"index.max_result_window": 100000
+	},
+	"mappings": {
+		"properties": {
+			"id": {
+				"type": "keyword"
+			},
+			"account": {
+				"type": "keyword"
+			},
+			"block_number": {
+				"type": "long"
+			},
+			"block_timestamp": {
+				"type": "date"
+			},
+			"change_type": {
+				"type": "long"
+			},
+			"change_balance": {
+				"type": "long"
+			},
+			"txid": {
+				"type": "long"
+			}
+		}
+	}
+}`
+
+}
